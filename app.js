cb3b3b3390c7f2e9278fc3b3ec6dc6d2f04cb54b
Patrick Puente <patrick.puente@bigcommerce.com>
2020-05-29 14:05:43 -0500

    PUT attribute values

--- app.js
+++ app.js
@@ -9,7 +9,6 @@ const axios = require('axios')
 const app = express()
 const router = express.Router()
 
-
 router.use(cors())
 router.use(bodyParser.json())
 router.use(awsServerlessExpressMiddleware.eventContext())
@@ -26,44 +25,47 @@ router.get('/customer-attributes', (req, res) => {
     // TODO: Build pagination to support 251+ attributes
     axios.get(attributesUrl)
         .then(attributesRes => {
-            console.log(attributesRes)
             if (!req.apiGateway.event.requestContext.authorizer.bcCustomerId)
                 throw new Error('No BC Customer ID in the request')
             if (!attributesRes.data.data || !attributesRes.data.meta)
                 throw new Error('BC Response did not include both data and meta.')
-
-            const customerId = req.apiGateway.event.requestContext.authorizer.bcCustomerId
-            const attributeValuesUrl = `https://api.bigcommerce.com/stores/${storeHash}/v3/customers/attribute-values?customer_id:in=${customerId}&limit=250`
-            axios.get(attributeValuesUrl)
-                .then(attributeValuesRes => {
-                    const resBody = {
-                        status: 200,
-                        data: attributesRes.data.map(attribute => {
-                            const { name, type } = attribute
-                            const value = attributeValuesRes.data.find(attrValue => attrValue.attribute_id === attribute.id)
-                            const { attribute_value, date_modified, date_created } = value
-                            return {
-                                attribute_id: attribute.id,
-                                name,
-                                type,
-                                attribute_value,
-                                date_created,
-                                date_modified,
-                            }
-                        })
-                    }
-                    res.status(200).json(resBody)
-                })
-                .catch(err => {
-                    console.error(err)
-                    res.status(500).json({ status: 500, data: { message: 'Error retrieving attribute values' } })
-                })
-            // res.status(200).json(bcRes)
+            res.status(200).json(attributesRes.data)
         })
         .catch(err => {
             console.error(err)
             res.status(500).json({ status: 500, data: { message: 'Error retrieving attributes' } })
         })
+})
+
+router.put('/customer-attribute-values', (req, res) => {
+    try {
+        if (!req.apiGateway.event.requestContext.authorizer.bcCustomerId)
+            throw new Error('No BC Customer ID in the request')
+        const customerId = parseInt(req.apiGateway.event.requestContext.authorizer.bcCustomerId)
+        const url = `https://api.bigcommerce.com/stores/${storeHash}/v3/customers/attribute-values`
+        const reqValues = req.body
+        const newValues = reqValues.map(value => {
+            value.customer_id = customerId
+            return value
+        })
+        console.log(newValues)
+        axios.put(url, newValues)
+            .then(attributeValuesRes => {
+                if (!attributeValuesRes.data.data || !attributeValuesRes.data.meta) {
+                    console.log(attributeValuesRes)
+                    throw new Error('BC attributeValuesResponse did not include both data and meta.')
+                }
+                console.log(attributeValuesRes.data)
+                res.status(200).json(attributeValuesRes.data)
+            })
+            .catch(err => {
+                console.error(err)
+                res.status(500).json({ status: 500, data: { message: 'Error updating attributes' } })
+            })
+    } catch (err) {
+        console.error(err)
+        res.status(400).json({ status: 400, data: { message: 'Bad request' } })
+    }
 })
 
 app.use('/', router)
